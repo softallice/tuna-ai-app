92ba934d1544bcc1fa07d3049eb78280be60ae2d
s991045 <s991045@gmail.com>
2021-08-18 11:02:19 +0900

    화면과 restapi 연결

--- src/pages/Xrays/Xray.vue
+++ src/pages/Xrays/Xray.vue
@@ -14,25 +14,45 @@
         enter-active-class="animated fadeIn slower"
         leave-active-class="animated fadeOut slower"
       >
+        <!-- 진단할 이미지 -->
         <div
           v-if="image"
+          class="row justify-center"
         >
+          <div class="text-h5 q-mb-md">{{ image.caption }}</div>
           <q-img
             :src="image.url"
             class="no-pointer-events"
             no-transition
             no-spinner
+            style="width:360px"
           />
           <div class="q-pa-lg">
             <q-btn 
+              class="btn-fixed-width"
               color="primary" 
-              label="Primary" 
+              label="진단" 
               @click="loadData"
             />
-            <div class="text-h5 q-mb-md">{{ image.caption }}</div>
-            <p>애국가의 가사는 1900년대 초에 쓰여졌다. 작사자는 크게 윤치호라는 설과 안창호라는 설 두 가지가 있으며, 국사편찬위원회의 공식적인 입장으로는 미상이다. 작사자 윤치호 설은 윤치호가 애국가의 가사를 1907년에 써서 후에 그 자신의 이름으로 출판했다는 것이다. 한편 안창호가 썼다는 주장은 안창호가 애국가를 보급하는 데에 앞장섰다는 데에 중점을 두고 있다. 1908년에 출판된 가사집 《찬미가》에 수록된 것을 비롯한 많은 일제 강점기의 애국가 출판물은 윤치호를 작사자로 돌리고 있는 등 윤치호 설에는 증거가 많은 반면[4] 안창호 설에는 실증적인 자료가 부족하다.</p>
+          </div>
+
+          <!-- 진단 결과 -->
+          <div
+            v-if="data"
+            class="row justify-center"
+          >
+            <div class="text-h5 q-mb-md">진단명 : {{ data.name }} ( {{ data.rate }})</div>
+            <q-img
+              :src="data.img_url"
+              class="no-pointer-events"
+              no-transition
+              no-spinner
+              style="width:360px"
+            />
           </div>
         </div>
+        
+        
       </transition>
     </page-body>
   </page>
@@ -44,18 +64,21 @@ import { useRoute } from 'vue-router'
 import store from 'src/doctorStore/xray.js'
 import { axios, api } from 'boot/axios'
 import { useQuasar } from 'quasar'
-// import fs from 'fs-extra'
 
 export default {
   name: 'Xray',
   setup() {
+    // 이미지를 읽어 들여 버퍼에서 변환 처리
     var Buffer = require('buffer/').Buffer
 
     const $q = useQuasar() 
     const data = ref(null)
+    // 이미지를 담기 위한 변수 선언
     const file = ref(null)
     let image = ref()
     
+    // 브라우저에서는 fs 모듈을 사용 불가
+    // axios로 이미지 읽어 들인 후 base 64로 변환 처리
     async function getBase64(url) {
       return axios
         .get(url, {
@@ -64,60 +87,72 @@ export default {
         .then(response => Buffer.from(response.data, 'binary').toString('base64'))
     }
 
-    file.value = getBase64('/images/xrays/2.png')
+    // 이미지 담기
+    // file.value = getBase64('/images/xrays/2.png')
     
+    // 해당 페이지를 오픈 할 경우 활성화
     onActivated(() => {
       let route = useRoute()
       image.value = store.getters.getImage(route.params.id)
+      
     })
-
+    // 해당 페이지를 닫을 경우 이미지 변수 null 처리
     onDeactivated(() => {
       image.value = null
+      data.value = null
     })
+
     
+    function getFileName(fileUrl) {
+      // URL을 가져와서 '/' 기준으로 배열 분리
+      var filePathSplit = fileUrl.split('/');
+      // 배열 분리 후 마지막 배열에서 다시 '.' 기준으로 분리
+      var fileNameSplit = filePathSplit[filePathSplit.length -1].split('.')
+      // 해당 파일의 첫번째 배열값 반환
+      var fileName = fileNameSplit[0]
+
+      return fileName
+    }
+
+    // 데이터 불러들여 처리
     async function loadData () {
-      const file = await getBase64('/images/xrays/1.jpg')
+      // 이미지 담기
+      // const file = await getBase64('/images/xrays/1.jpg')
+      // url로 접근해서 처리하는 방식이라 파일명을 함수로 못가져옴
+      // 잘라서 가져오는 함수 구현
+      const fileName = getFileName(image.value.url)
+      const file = await getBase64(image.value.url)
       const base64 = 'data:image/jpg;base64,' + file;
       const blob = await fetch(base64).then(res => res.blob())
 
       // console.log('base64 ======> ', base64)
       // console.log('blob ======> ', blob)
       const formData = new FormData() 
-      formData.append('file', blob);
+      // form에 파일 정보 담기
+      // 이미지 파일과 파일명
+      formData.append('file', blob); 
+      formData.append('fileName', fileName);
 
-
+      // rest api로 예측 요청 넘기기
       api.post('/predict', formData , { 
         headers: {
           'Content-Type': 'multipart/form-data'
         }
        })
         .then((response) => {
+          // rest-api로 보낸 이미지 예측 결과값 받아오기
           console.log('response.data : ', response.data)
           data.value = response.data
         })
         .catch((e) => {
-          console.log('error : ', e)
+          // 처리 오류의 경우 화면에 notice
           $q.notify({
             color: 'negative',
             position: 'top',
-            message: 'x-ray 예측이 안되었습니다.',
+            message: 'x-ray 예측이 실패하였습니다.',
             icon: 'report_problem'
           })
         })
-      // api.post(config)
-      //   .then((response) => {
-      //     console.log('response.data : ', response.data)
-      //     data.value = response.data
-      //   })
-      //   .catch((e) => {
-      //     console.log('error : ', e)
-      //     // $q.notify({
-      //     //   color: 'negative',
-      //     //   position: 'top',
-      //     //   message: 'Loading failed',
-      //     //   icon: 'report_problem'
-      //     // })
-      //   })
     }
   
 
@@ -130,3 +165,7 @@ export default {
   }
 }
 </script>
+<style lang="sass" scoped>
+.btn-fixed-width
+  width: 200px
+</style>
